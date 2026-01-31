7f28647ba45bc360c5d87c6c925180f2aff953dc
DistinguishAJ <distinguishjosiaha@gmail.com>
2026-01-31 21:41:16 +0100

    golang commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var greeting string
+	greeting = "Hello, World!"
+
+	fmt.Println(greeting)
+
+	var count int
+	count = 50
+	fmt.Println(count)
+
+	var isRuning bool
+	isRuning = true
+	fmt.Println(isRuning)
+
+	var (firstName, lastName string)
+	firstName = "John"
+	lastName = "Doe"
+	fmt.Println(firstName, lastName)
+
+	email := "john.doe@example.com"
+	fmt.Println(email)
+
+	age := "30"
+	fmt.Println(age)
+
+	var year = 2026
+	fmt.Println(year)
+	
+	}
